a211c4da5680c53c98b124e277b8daf339d1a39a
ramimhossain12 <ramimhossain6242@gmail.com>
2021-09-08 01:28:43 +0600

    fetch Data

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'single_todo.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,81 +9,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final _textFielt = TextEditingController();
-  List<String> todos = [
-    'Test ToDO 1',
-    'Test ToDO 2',
-    'Test ToDO 3',
-    'Test ToDO 4',
-    'Test ToDO 5'
-  ];
- void _addtodo(){
-   if(_textFielt.text.length <=0){
-     return;
-   }
 
-   setState(() {
-     todos.insert(0, _textFielt.text);
-   });
-   _textFielt.text='';
-   Navigator.of(context).pop();
-   print(_textFielt);
- }
+  Future<void> _addtodo() async {
+    if (_textFielt.text.length <= 0) {
+      return;
+    }
+  
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("NOTE APP"),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, i) => SingleTodo(todo: todos[i]),
-        itemCount: todos.length,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("todo").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text('No Data Found'),
+            );
+          }
+          return ListView(
+            children: snapshot.data!.docs
+                .map((todo) => SingleTodo(todo: todo.data().toString()))
+                .toList(),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showDialog(context: context, builder: (context){
-            return AlertDialog(
-              title: Text("Add Data"),
-              content: TextField(
-                controller: _textFielt,
-                decoration:  InputDecoration(
-                  hintText: "Add a Todo",
-
-
-                ),
-              ),
-              actions: [
-                RaisedButton(onPressed: (){
-
-                  Navigator.of(context).pop();
-                },
-                  color: Colors.red,
-                  child: Text("Cancel"),
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(25.0),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Add Data"),
+                  content: TextField(
+                    controller: _textFielt,
+                    decoration: InputDecoration(
+                      hintText: "Add a Todo",
+                    ),
                   ),
-
-                ),
-                RaisedButton(onPressed: (){
-
-                  _addtodo();
-                },
-                  color: Colors.green,
-                  shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(25.0),
-                  ),
-
-                  child: Text("Add"),
-                ),
-              ],
-            );
-          });
+                  actions: [
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      color: Colors.red,
+                      child: Text("Cancel"),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        _addtodo();
+                      },
+                      color: Colors.green,
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                      ),
+                      child: Text("Add"),
+                    ),
+                  ],
+                );
+              });
         },
         child: Icon(Icons.add),
       ),
     );
   }
 }
+
+//video start 26.7
